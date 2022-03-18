2840d457b702c5971085f9abc646e3f5b27d41c1
Zadigo <pendenquejohn@gmail.com>
2022-03-18 07:13:25 +0100

    Fixtures for testing

--- collection/tests.py
+++ collection/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.test.client import Client, RequestFactory
 
-# Create your tests here.
+from collection import views
+
+
+class TestCollectionApi(TestCase):
+    fixtures = ['products.json', 'collections.json']
+    
+    def setUp(self):
+        self.factory = RequestFactory()
+    
+    def test_get_all_collection(self):
+        request = self.factory.get('/api/v1/collection/all')
+        response = views.collecion_view(request, 'all')
+        data = response.data
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(len(data['results']), 1)
+    
+    def test_pants_collection(self):
+        request = self.factory.get('/api/v1/collection/shorts')
+        response = views.collecion_view(request, 'all')
+        data = response.data
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(len(data['results']), 1)
+        
--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.test.client import Client, RequestFactory
 
-# Create your tests here.
+from shop import views
+
+class TestShopApi(TestCase):
+    fixtures = ['products.json']
+    
+    def test_products_view_structure(self):
+        factory = RequestFactory()
+        request = factory.get('/api/v1/shop/products')
+        response = views.products_view(request)
+        self.assertEqual(response.status_code, 200)
+        expected_data = {
+            'count': 0,
+            'infos': {
+                'total_count': 0,
+                'filters': {
+                    'colors': []
+                }
+            },
+            'next': None,
+            'previous': None,
+            'results': []
+        }
+        self.assertDictEqual(response.data, expected_data)
