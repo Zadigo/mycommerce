b5af2ee74bb921aa86050e5d59488bd83cca3d62
Zadigo <pendenquejohn@gmail.com>
2022-06-25 15:52:02 +0200

    Vue CLI

--- mycommerce/settings.py
+++ mycommerce/settings.py
@@ -133,13 +133,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     Path.joinpath(BASE_DIR, 'static'),
-    Path.joinpath(FRONT_DIR, 'dist')
+    Path.joinpath(FRONT_DIR, 'dist/static')
 ]
 
 MEDIA_URL = 'media/'
