4f335826d126beb164e01223147d276757e700fa
Zadigo <pendenquejohn@gmail.com>
2022-06-25 16:31:47 +0200

    Vue CLI

--- front2/src/i18n.js
+++ front2/src/i18n.js
@@ -2,15 +2,15 @@ import { createI18n } from 'vue-i18n'
 
 function loadLocaleMessages () {
   const messages = {}
-  // const locales = require.context('./', true, /[A-Za-z0-9-_,\s]+\.json$/i)
+  const locales = require.context('./', true, /[A-Za-z0-9-_,\s]+\.json$/i)
 
-  // locales.keys().forEach(key => {
-  //   const matched = key.match(/([A-Za-z0-9-_]+)\./i)
-  //   if (matched && matched.length > 1) {
-  //     const locale = matched[1]
-  //     messages[locale] = locales(key)
-  //   }
-  // })
+  locales.keys().forEach(key => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+    if (matched && matched.length > 1) {
+      const locale = matched[1]
+      messages[locale] = locales(key)
+    }
+  })
 
   return messages
 }
--- front2/vue.config.js
+++ front2/vue.config.js
@@ -1,6 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+// const dotenv = require('dotenv-webpack')
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      // new dotenv({
+      //   path: './.env',
+      //   allowEmptyValues: false,
+      //   silent: false,
+      //   prefix: 'import.meta.env'
+      // })
+    ]
+  },
   outputDir: './dist/',
   assetsDir: 'static'
 })
