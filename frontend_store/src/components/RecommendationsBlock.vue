49837a21109737a031bd7b2ef7f8ffcdf40e1173
Zadigo <pendenquejohn@gmail.com>
2024-07-16 16:41:21 +0200

    Code refactoring

--- frontend_store/src/components/RecommendationsBlock.vue
+++ frontend_store/src/components/RecommendationsBlock.vue
@@ -1,6 +1,6 @@
 <template>
   <div :data-count="quantity" class="recommendations">
-    <h2 class="h4 text-center mb-5">{{ blockTitle }}</h2>
+    <h2 class="h4 text-center mb-5">{{ $t(blockTitle) }}</h2>
 
     <div ref="productsRow" class="row g-1">
       <div v-for="product in recommendations" :key="product.id" :class="rowClass">
@@ -12,12 +12,11 @@
 
 <script>
 import { ref } from 'vue'
-import { client } from '../plugins/axios'
-import { useVueSession } from 'src/plugins/vue-storages'
-// import { createMockupProducts } from 'src/utils'
-
-import ProductCard from 'components/products/ProductCard.vue'
 import { useRoute } from 'vue-router'
+import { client } from 'src/plugins/axios'
+import { useVueSession } from 'src/plugins/vue-storages'
+
+import ProductCard from 'src/components/products/ProductCard.vue'
 
 export default {
   name: 'RecommendationsBlock',
@@ -57,7 +56,6 @@ export default {
             q: props.quantity
           }
         })
-        // recommendations.value = createMockupProducts(app.props.quantity)
         recommendations.value = response.data
         session.expire('recommendations', response.data, 100)
       } catch (e) {
