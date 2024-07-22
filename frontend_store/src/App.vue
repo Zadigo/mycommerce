cd735231bd96decb1a46f669382cf4f638254a57
Zadigo <pendenquejohn@gmail.com>
2024-07-22 23:47:23 +0200

    Language selection

--- frontend_store/src/App.vue
+++ frontend_store/src/App.vue
@@ -5,6 +5,38 @@
         <component :is="Component" />
       </transition>
     </router-view>
+
+    <!-- Modals -->
+    
+    <v-bottom-sheet id="language-modal" v-model="shopStore.showLanguageModal" :persistent="languageModalIsPersistent">
+      <v-card>
+        <div class="container p-5">
+          <div class="row">
+            <div class="col-6">
+              <p class="fw-bold">{{ $t('Sélectionnez votre emplacement') }}</p>
+              <v-autocomplete v-model="languageOptions.location" :items="countries" variant="solo-filled" flat>
+                <v-text-field type="text"></v-text-field>
+              </v-autocomplete>
+            </div>
+            
+            <div class="col-6">
+              <p class="fw-bold">{{ $t('Sélectionnez votre langue') }}</p>
+
+              <div class="d-flex gap-1">
+                <v-btn :active="languageOptions.language === 'fr'" variant="outlined" rounded @click="languageOptions.language = 'fr'">FR</v-btn>
+                <v-btn :active="languageOptions.language === 'en'" variant="outlined" rounded @click="languageOptions.language = 'en'">EN</v-btn>
+              </div>
+            </div>
+
+            <div class="col-12 d-flex justify-content-end">
+              <v-btn variant="tonal" color="primary" rounded @click="handleLanguageSelection">
+                {{ $t('Enregistrer mon choix') }}
+              </v-btn>
+            </div>
+          </div>
+        </div>
+      </v-card>
+    </v-bottom-sheet>
   </v-app>
 </template>
 
@@ -17,6 +49,7 @@ import { useDocumentVisibility, useMediaQuery, useScreenOrientation } from '@vue
 import { useSchemaOrg, defineOrganization } from '@unhead/schema-org'
 
 import organization from 'src/data/organization.json'
+import countries from 'src/data/countries.json'
 
 export default {
   name: 'App',
@@ -30,6 +63,12 @@ export default {
     
     const shopStore = useShop()
     const authenticationStore = useAuthentication()
+
+    const languageModalIsPersistent = ref(false)
+    const languageOptions = ref({
+      location: 'France',
+      language: 'fr'
+    })
     
     provide('currentLanguage', currentLanguage)
     
@@ -48,7 +87,10 @@ export default {
     ])
 
     return {
+      countries,
       shopStore,
+      languageModalIsPersistent,
+      languageOptions,
       authenticationStore
     }
   },
@@ -57,7 +99,22 @@ export default {
     this.shopStore.loadFromCache()
   },
   beforeMount () {
-    this.currentLanguage = this.sessionStorageData.language || 'fr'
+    if (!this.$session.keyExists('lang')) {
+      this.languageModalIsPersistent = true
+      this.shopStore.showLanguageModal = true
+    }
+    // this.currentLanguage = this.sessionStorageData.language || 'fr'
+  },
+  methods: {
+    /**
+     * Allows the user to select his preferred language
+     * and location 
+     */
+    handleLanguageSelection () {
+      this.$session.create('lang', this.languageOptions)
+      this.languageModalIsPersistent = false
+      this.shopStore.showLanguageModal = false
+    }
   }
 }
 </script>
