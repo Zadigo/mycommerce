5898e6593f7f209ea1735bfd8f1ee3bee1dca1b4
Zadigo <pendenquejohn@gmail.com>
2024-07-23 12:24:37 +0200

    Improve messages layout

--- frontend_store/src/components/BaseMessages.vue
+++ frontend_store/src/components/BaseMessages.vue
@@ -1,15 +1,15 @@
 <template>
-  <div v-if="messagesStore.hasMessages" id="alerts" class="row mb-2">
+  <div v-if="messagesStore.hasMessages" id="alerts" class="row mb-1" style="margin-top: 3.5rem;">
     <div class="col-sm-12 col-md-9 offset-md-2">
       <transition-group name="opacity">
-        <v-alert v-for="messageItem in messageItems" :key="messageItem.id" :text="messageItem.message" :type="messageItem.type" :title="messageItem.title" class="mb-1" variant="tonal" closable></v-alert>
+        <v-alert v-for="messageItem in messageItems" :key="messageItem.id" :text="messageItem.message" :type="messageItem.type" :title="messageItem.title" class="mb-1" variant="tonal" closable @close="removeMessage(messageItem)"></v-alert>
       </transition-group>
     </div>
   </div>
 </template>
 
 <script>
-import { storeToRefs } from 'pinia'
+import { mapActions, storeToRefs } from 'pinia'
 import { useMessages } from 'src/stores/messages'
 
 export default {
@@ -17,10 +17,14 @@ export default {
   setup() {
     const messagesStore = useMessages()
     const { messageItems } = storeToRefs(messagesStore)
+
     return {
       messagesStore,
       messageItems
     }
-  } 
+  },
+  methods: {
+    ...mapActions(useMessages, ['removeMessage'])
+  }
 }
 </script>
