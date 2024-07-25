e1daaecbc97fa48262bcafc15dac1915d3339024
Zadigo <pendenquejohn@gmail.com>
2024-07-25 23:48:44 +0200

    Allow message deletion directly

--- frontend_store/src/components/BaseMessages.vue
+++ frontend_store/src/components/BaseMessages.vue
@@ -2,7 +2,7 @@
   <div v-if="messagesStore.hasMessages" id="alerts" class="row mb-1" style="margin-top: 3.5rem;">
     <div class="col-sm-12 col-md-9 offset-md-2">
       <transition-group name="opacity">
-        <v-alert v-for="messageItem in messageItems" :key="messageItem.id" :text="messageItem.message" :type="messageItem.type" :title="messageItem.title" class="mb-1" variant="tonal" closable @close="removeMessage(messageItem)"></v-alert>
+        <v-alert v-for="messageItem in messageItems" :key="messageItem.id" :text="messageItem.message" :type="messageItem.type" :title="messageItem.title" class="mb-1" variant="tonal" closable @click:close="removeMessage(messageItem)"></v-alert>
       </transition-group>
     </div>
   </div>
