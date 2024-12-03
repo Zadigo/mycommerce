59845c461051f47e28cbb5b1b41b098437fa27b2
Zadigo <pendenquejohn@gmail.com>
2024-12-03 20:29:41 +0100

    Code refactoring

--- mystore/accounts/models.py
+++ mystore/accounts/models.py
@@ -1,3 +1,4 @@
+import stripe
 from accounts.choices import Genders
 from django.contrib.auth.models import User
 from django.db import models
--- mystore/accounts/tasks.py
+++ mystore/accounts/tasks.py
@@ -1,11 +1,14 @@
+import stripe
 from celery import shared_task
 
 
 @shared_task
 def login_workflow():
+    # 1. Email user for login made or failed
     return NotImplemented
 
 
 @shared_task
 def signup_workflow():
+    # 1. Create stripe customer
     return NotImplemented
