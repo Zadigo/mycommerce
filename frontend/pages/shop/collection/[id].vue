f8b0ecf1bec3bc16d890ae2d8f879c65c1b83c5b
Zadigo <pendenquejohn@gmail.com>
2025-01-09 17:31:36 +0100

    Code refactoring

--- frontend/pages/shop/collection/[id].vue
+++ frontend/pages/shop/collection/[id].vue
@@ -5,8 +5,8 @@
       <div class="col-12">
         <div class="card shadow-none">
           <div class="card-body pb-1 d-flex flex-row justify-content-start">
-            <h1 :aria-labelledby="$route.params.id as string" class="text-uppercase fw-bold h4">
-              {{ $route.params.id }}
+            <h1 :aria-labelledby="route.params.id" class="text-uppercase fw-bold h4">
+              {{ route.params.id }}
             </h1>
           </div>
         </div>
@@ -30,10 +30,14 @@
 
 <script lang="ts" setup>
 import { useChangeCase } from '@vueuse/integrations/useChangeCase'
-// import { useScroll } from '@vueuse/core'
 import type { Product } from '~/types'
 
 const route = useRoute()
+
+const productsLoading = ref(true)
+const products = ref<Product[]>([])
+
+provide('productsLoading', productsLoading)
 
 useHead({
   title: useChangeCase(route.params.id as string, 'capitalCase'),
@@ -45,17 +49,10 @@ useHead({
   ]
 })
 
-// const { x } = useScroll(window, { behavior:'smooth' })
-
-const productsLoading = ref(true)
-const products = ref<Product[]>([])
-
 const AsyncFeed = defineAsyncComponent({
   loader: async () => import('~/components/products/Feed.vue'),
   timeout: 10000
 })
-
-provide('productsLoading', productsLoading)
 
 function handleLoadedProducts(data: Product[]) {
   productsLoading.value = false
