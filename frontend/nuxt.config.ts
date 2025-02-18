afe8db2cbe036712e24ac2e4c8ee960604c963ba
Zadigo <pendenquejohn@gmail.com>
2025-02-18 18:59:37 +0100

    Base modules

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/sitemap',
+    '@unlok-co/nuxt-stripe',
+    '@nuxt/image',
+    '@nuxtjs/i18n',
+    'vuetify-nuxt-module',
+    '@nuxt/test-utils'
+  ]
 })
