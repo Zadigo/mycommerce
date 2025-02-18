b56edcde77e510b15562779cc94d23ded649eea6
Zadigo <pendenquejohn@gmail.com>
2025-02-18 22:08:56 +0100

    Add types

---
+++ frontend/types/nuxt.d.ts
@@ -0,0 +1,13 @@
+// types/nuxt.d.ts
+
+// import type { AxiosInstance } from 'axios'
+
+// declare module '#app' {
+//     interface NuxtApp {
+//         $client: AxiosInstance
+//     }
+
+//     interface ComponentCustomProperties {
+//         $client: AxiosInstance
+//     }
+// }
