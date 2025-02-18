8ebd304aed436581b24c4205ce30f51eadd27f07
Zadigo <pendenquejohn@gmail.com>
2025-02-18 23:53:14 +0100

    Dayjs + Firebase

---
+++ frontend/plugins/firedb.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
+
+export default defineNuxtPlugin(nuxtApp => {
+    const config = useRuntimeConfig()
+    const app = initializeApp({
+        apiKey: config.public.firebaseApiKey,
+        authDomain: config.public.firebaseAuthDomain,
+        databaseURL: config.public.firebaseDatabaseUrl,
+        storageBucket: config.public.firebaseStorageBucket,
+        appId: config.public.firebaseAppId,
+        projectId: config.public.firebaseProjectId,
+        measurementId: config.public.firebaseMeasurementId,
+        messagingSenderId: config.public.firebaseMessagingSenderId
+    })
+    const db = getDatabase(app)
+
+    nuxtApp.provide('fireApp', app)
+    nuxtApp.provide('fireDb', db)
+})
