defb83d84f637afe8560ebc16576618acf5c7c2b
Zadigo <pendenquejohn@gmail.com>
2025-02-20 15:20:21 +0100

    Implement authentication

---
+++ frontend/composables/authentication.ts
@@ -0,0 +1,77 @@
+import type { StringNull } from "~/types"
+import createDjangoClient, { type LoginApiResponse } from '~/composables/django_client'
+
+export function useAuthencationComposable() {
+    const nuxtApp = tryUseNuxtApp()
+    
+    const email = ref<StringNull>('')
+    const password = ref<StringNull>('')
+    const authenticationFailuresCounter = ref(0)
+
+    /**
+     * Base entry function used to request an authentication
+     * access/refresh token in order to login the user 
+     */
+    async function authenticate(path: string, callback: (data: LoginApiResponse) => void) {
+        try {
+            if (nuxtApp) {
+                const authClient = createDjangoClient('/auth/v1/')
+
+                const response = await authClient.post<LoginApiResponse>(path, {
+                    username: email.value,
+                    email: email.value,
+                    password: password.value
+                })
+    
+                authenticationFailuresCounter.value = 0
+    
+                callback.call(nuxtApp, response.data)                
+            }
+        } catch {
+            authenticationFailuresCounter.value += 1
+        }
+    }
+
+    async function login(callback: (data: LoginApiResponse) => void) {
+        await authenticate('/token/', callback)
+    }
+
+    async function refresh(callback: (data: LoginApiResponse) => void) {
+        await authenticate('/refesh/', callback)
+    }
+
+    async function logout(callback: () => void) {
+        try {
+            const accessToken = useCookie('access')
+            const refreshToken = useCookie('refresh')
+            
+            accessToken.value = null
+            refreshToken.value = null
+
+            callback.call(nuxtApp)
+        } catch {
+            // Do something
+        }
+    }
+
+    async function testLogin (callback: () => void) {
+        const accessToken = useCookie('access')
+        const refreshToken = useCookie('refresh')
+
+        accessToken.value = 'test token'
+        refreshToken.value = 'refresh token' 
+
+        callback()
+    }
+
+    return {
+        email,
+        password,
+        authenticationFailuresCounter,
+        testLogin,
+        authenticate,
+        login,
+        logout,
+        refresh
+    }
+}
