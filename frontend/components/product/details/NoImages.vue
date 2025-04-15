97cedbf37a259cd4785062000b07b49a03c0c375
Zadigo <pendenquejohn@gmail.com>
2025-04-16 01:07:34 +0200

    Use tailwind skeleton

--- frontend/components/product/details/NoImages.vue
+++ frontend/components/product/details/NoImages.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="product-images" class="grid grid-cols-2 grid-rows-2 gap-1 col-span-8">
+    <TailSkeleton class="w-full h-[600px] bg-gray-100 rounded-none" />
+    <TailSkeleton class="w-full h-[600px] bg-gray-100 rounded-none" />
+    <TailSkeleton class="w-full h-[600px] bg-gray-100 rounded-none" />
+    <TailSkeleton class="w-full h-[600px] bg-gray-100 rounded-none" />
+
+    <!-- <NuxtImg src="/placeholder.svg" :alt="product.name" class="w-full" />
     <NuxtImg src="/placeholder.svg" :alt="product.name" class="w-full" />
     <NuxtImg src="/placeholder.svg" :alt="product.name" class="w-full" />
-    <NuxtImg src="/placeholder.svg" :alt="product.name" class="w-full" />
-    <NuxtImg src="/placeholder.svg" :alt="product.name" class="w-full" />
+    <NuxtImg src="/placeholder.svg" :alt="product.name" class="w-full" /> -->
   </div>
 </template>
 
