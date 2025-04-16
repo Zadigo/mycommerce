18722eaedddd60b51e0794cb6c6566666bb3834e
Zadigo <pendenquejohn@gmail.com>
2025-04-16 20:32:28 +0200

    Image did not fill container

--- frontend/components/product/details/SixImages.vue
+++ frontend/components/product/details/SixImages.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="product-images" class="grid grid-cols-2 auto-rows-min gap-1 col-span-8">
     <div v-for="image in images" :key="image.id" id="image" class="cursor-zoom-in">
-      <NuxtImg :src="mediaPath(image.original, '/placeholder.svg')" :alt="image.name" class="cursor-zoom-in" @click="emit('zoom-image', image)" />
+      <NuxtImg :src="mediaPath(image.original, '/placeholder.svg')" :alt="image.name" class="cursor-zoom-in w-full" @click="emit('zoom-image', image)" />
     </div>
   </div>
 </template>
