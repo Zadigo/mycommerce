41a32700d2949000c572bc95a9c54087cf6f3481
Zadigo <pendenquejohn@gmail.com>
2025-04-16 16:19:33 +0200

    Add id to links

--- frontend/components/navbar/Base.vue
+++ frontend/components/navbar/Base.vue
@@ -11,38 +11,38 @@
       <div class="hidden lg:block">
         <ul class="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
           <li class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-            <v-btn class="ms-auto me-2" variant="tonal" color="dark" rounded @click="shopStore.showSearchModal=true">
+            <v-btn id="action-search" class="ms-auto me-2" variant="tonal" color="dark" rounded @click="shopStore.showSearchModal=true">
               {{ $t('Rechercher') }}
             </v-btn>
           </li>
           <li v-if="!authStore.isAuthenticated" class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-            <a href="#" class="flex items-center gap-2" @click.prevent="handleShowCartDrawer">
+            <a id="action-cart-navbar" href="#" class="flex items-center gap-2" @click.prevent="handleShowCartDrawer">
               <Icon name="fa-solid:shopping-bag" size="18" />
               {{ $t("Panier") }}
             </a>
           </li>
           <li v-if="!authStore.isAuthenticated" class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-            <a href="#" class="flex items-center gap-2" @click.prevent="authStore.showLoginDrawer=true">
+            <a id="action-signin" href="#" class="flex items-center gap-2" @click.prevent="authStore.showLoginDrawer=true">
               <Icon name="fa-solid:sign-in-alt" size="18" />
               {{ $t('Se connecter') }}
             </a>
           </li>
           <li v-else class="flex items-center p-1 text-sm gap-2 text-slate-600">
-            <a href="#" class="flex items-center" @click.prevent="proxyLogout">
+            <a id="action-signout" href="#" class="flex items-center" @click.prevent="proxyLogout">
               <Icon name="fa-solid:sign-out-alt" size="18" />
               {{ $t('Se déconnecter') }}
             </a>
           </li>
           <li class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-            <NuxtLink id="link-account-home" to="/account/" class="flex items-center gap-2">
-            <Icon name="fa-solid:user" size="18" />
-            {{ $t('Compte') }}
-          </NuxtLink>
+            <NuxtLink id="link-account-navbar" to="/account/" class="flex items-center gap-2">
+              <Icon name="fa-solid:user" size="18" />
+              {{ $t('Compte') }}
+            </NuxtLink>
           </li>
         </ul>
       </div>
 
-      <button class="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:hidden" type="button">
+      <button id="action-menu" class="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:hidden" type="button">
         <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
           <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
