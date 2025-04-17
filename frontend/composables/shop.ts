a379d44378585c1b7f8189a1e91520b214902173
Zadigo <pendenquejohn@gmail.com>
2025-04-17 12:48:03 +0200

    Temporary modification of useshop

--- frontend/composables/shop.ts
+++ frontend/composables/shop.ts
@@ -1,65 +1,72 @@
 import type { Product, ProductImage } from "~/types"
+// import { isRef } from 'vue'
+import type { Ref } from 'vue'
 
-export function useShopComposable () {
-    const { $i18n } = useNuxtApp()
-    const isLiked = ref(false)
+export function useShopComposable() {
+  // const { $i18n } = useNuxtApp()
+  const isLiked = ref(false)
 
-    function validateProp<T extends Product | ProductImage>(item: T | object | null | undefined): item is T {
-        if (!item || typeof item !== 'object') {
-            return false
-        } else {
-            return (
-                'id' in item &&
-                typeof (item as T).id === 'number'
-            )
-        }
+  function validateProp<T extends Product | ProductImage>(item: T | object | null | undefined): item is T {
+    if (!item || typeof item !== 'object') {
+      return false
+    } else {
+      return (
+        'id' in item &&
+        typeof (item as T).id === 'number'
+      )
     }
-    
-    /**
-     * A composable that implements default
-     * resusable functions for the shop, such
-     * as liking a product or adding it to
-     * the user cart
-     */
-    function translatePrice(value: string | number | undefined) {
-        if (value) {
-            let price
+  }
 
-            if (typeof value === 'string') {
-                price = parseFloat(value)
-            } else {
-                price = value
-            }
-            
-            return $i18n.n(price, 'currency', $i18n.locale)
-        } else {
-            return '0'
-        }
-    }
+  /**
+   * A composable that implements default
+   * resusable functions for the shop, such
+   * as liking a product or adding it to
+   * the user cart
+   */
+  function translatePrice(value: string | number | undefined | Ref) {
+    return value
+    // if (isRef<string | number>(value)) {
+    //   price = value.value
+    // }
 
-    /**
-     * Main entry function for managing the user liked
-     * products either locally or in the backend.
-     * Handles the action of liking a product
-     * and therefore adding it to the user's
-     * wishlist
-     */
-    function handleLike(items: number[], product: Product | null | undefined): number[] {
-        if (product) {
-            if (items.includes(product.id)) {
-                items.push(product.id)
-            } else {
-                const index = items.findIndex(x => product.id)
-                items.splice(index, 1)
-            }
-        }
-        return items
-    }
+    // if (value) {
+    //   let price
 
-    return {
-        isLiked,
-        validateProp,
-        translatePrice,
-        handleLike
+    //   if (typeof value === 'string') {
+    //     price = parseFloat(value)
+    //   } else {
+    //     price = value
+    //   }
+
+    //   return $i18n.n(price, 'currency', $i18n.locale)
+    // } else {
+    //   return '0'
+    // }
+  }
+
+  /**
+   * Main entry function for managing the user liked
+   * products either locally or in the backend.
+   * Handles the action of liking a product
+   * and therefore adding it to the user's
+   * wishlist
+   */
+  function handleLike(items: number[], product: Product | null | undefined): number[] {
+    if (product) {
+      if (items.includes(product.id)) {
+        items.push(product.id)
+      } else {
+        const index = items.findIndex(x => product.id)
+        items.splice(index, 1)
+      }
     }
+    return items
+  }
+
+  return {
+    isLiked,
+    validateProp,
+    translatePrice,
+    handleLike
+  }
 }
