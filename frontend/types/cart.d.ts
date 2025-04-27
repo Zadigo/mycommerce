ade4697359db4ae992b5f73ff595f2dff5f91f64
Zadigo <pendenquejohn@gmail.com>
2025-04-27 20:02:12 +0200

    Update delivery option types

--- frontend/types/cart.d.ts
+++ frontend/types/cart.d.ts
@@ -47,6 +47,31 @@ export interface ProductToEdit extends CartStatistic {
 }
 
 export interface DeliveryOption {
-    id: number
+    id: string
     name: string
+    description: string
+    estimated_arrival_time: null
+    default: boolean
+    shipping_cost: string | number
+    selected: boolean
+    is_relais: boolean
+    is_home: boolean
+    is_store: boolean
+    is_inpost: boolean
+    is_managed_by_kbrw: boolean
+    is_cash_on_delivery: boolean
+    is_tAH: boolean
+    estimated_delivery_date: {
+        delivery_date: string
+        minDelivery_date: string
+        maxDelivery_date: string
+    }
+    shipping_cost_value: number
+    shop_runback_values: {
+        is_link_order_return_active: boolean
+        order_return_type: {}
+        order_return_link: string | null
+        order_return_number_of_day: number
+        url: string
+    }
 }
