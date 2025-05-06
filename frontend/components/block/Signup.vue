633fa6770a5d8c9bc73208613b69127dc4f71877
Zadigo <pendenquejohn@gmail.com>
2025-05-07 00:49:35 +0200

    Translation plus improve signup process

--- frontend/components/block/Signup.vue
+++ frontend/components/block/Signup.vue
@@ -5,19 +5,33 @@
     </h3>
     
     <form @submit.prevent>
-      <TailInput v-model="requestData.email" :placeholder="$t('Email')" type="email" class="mb-2" autocomplete="email" />
-      <TailInput v-model="requestData.password" :placeholder="$t('Mot de passe')" type="password" class="mb-5" autocomplete="new-password" flat />
+      <TailInput v-model="requestData.email" :placeholder="$t('Email')" type="email" autocomplete="email" />
+      <TailInput v-model="requestData.password1" :placeholder="$t('Mot de passe')" type="password" class="my-2" autocomplete="new-password" />
+      <TailInput v-model="requestData.password2" :placeholder="$t('Mot de passe')" type="password" class="mb-5" autocomplete="new-password" />
       
       <p class="font-light text-slate-500 text-sm">
         {{ $t('Signup: Password constraints') }}
       </p>
       
       <div class="my-5">
-        <v-checkbox v-model="requestData.newsletter" label="Je veux recevoir les nouveautés et des communications commerciales personnalisées de BERSHKA par e-mail ou d'autres moyens." />
-        <v-checkbox v-model="acceptPrivacy" label="J'ai lu et j'accepte les Conditions générales  et je comprends les informations sur le traitement de mes données personnelles expliquées dans la Politique de confidentialité" />
+        <div class="flex items-center space-x-2">
+          <TailCheckbox v-model="requestData.newsletter" id="action-novelty-newsletter" />
+          <TailLabel>
+            {{ $t("Je veux recevoir les nouveautés et des communications commerciales personnalisées de BERSHKA par e-mail ou d'autres moyens") }}
+          </TailLabel>
+        </div>
+
+        <div class="flex items-center space-x-2 mt-5">
+          <TailCheckbox v-model="acceptPrivacy" id="action-accept-conditions" />
+          <TailLabel>
+            <p>
+              {{ $t("J'ai lu et j'accepte les Conditions générales et je comprends les informations sur le traitement de mes données personnelles expliquées dans la") }} <RouterLink to="/conditions-generales">{{ $t("politique de confidentialité") }}</RouterLink>
+            </p>
+          </TailLabel>
+        </div>
       </div>
 
-      <TailButton id="signup-email" size="lg" class="w-full" @click="handleSignup">
+      <TailButton id="action-signup-email" :disabled="!acceptPrivacy" size="lg" class="w-full" @click="handleSignup">
         {{ $t('Créer un compte') }}
       </TailButton>
     </form>
@@ -29,14 +43,21 @@ import { z } from 'zod'
 
 const RequestDataSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(8),
-  newsletter: z.boolean()
+  password1: z.string().min(8).refine(x => {
+    console.log('RequestDataSchema', x)
+    return x
+  }),
+  password2: z.string().min(8).refine(x => {
+    console.log('RequestDataSchema', x)
+    return x
+  }),
+  newsletter: z.boolean().default(false)
 })
 
 type RequestData = z.infer<typeof RequestDataSchema>
 
 const emit = defineEmits({
-  authenticate () {
+  authenticate() {
     return true
   }
 })
@@ -44,26 +65,31 @@ const emit = defineEmits({
 const acceptPrivacy = ref<boolean>(false)
 const requestData = ref<RequestData>({
   email: '',
-  password: '',
+  password1: '',
+  password2: '',
   newsletter: false
 })
 
-const canSignup = computed(() => {
-  return useArrayEvery([acceptPrivacy], (x) => x === true)
-  // return [acceptPrivacy.value].every(x => x !== true)
+const { execute } = useFetch('/api/v1/signup', {
+  method: 'POST',
+  body: requestData.value,
+  immediate: false,
+  watch: false
 })
 
-async function handleSignup() {
-  RequestDataSchema.parse(requestData.value)
+/**
+ * Signup the user
+ */
+const handleSignup = async () => {
+  console.log('Signup')
+  await RequestDataSchema.parseAsync(requestData.value)
+  
+  execute()
 
-  if (canSignup.value) {
-    $fetch('/api/v1/signup', {
-      method: 'POST',
-      body: requestData.value
-    })
-    emit('authenticate')
-  } else {
-    // TODO: Show error message
-  }
+  requestData.value.email = ''
+  requestData.value.password1 = ''
+  requestData.value.password2 = ''
+  
+  emit('authenticate')
 }
 </script>
--- frontend/i18n/locales/es-ES.ts
+++ frontend/i18n/locales/es-ES.ts
@@ -155,6 +155,8 @@ export default defineI18nLocale(async locale => {
         "Login: Privacy Policy": "En me connectant avec mon identifiant social, j'accepte de lier mon compte conformément à la",
         "Créer un compte": "Créer un compte",
         "Signup: Password constraints": "8 caractères minimum, comprenant une minuscule, une majuscule et un chiffre. Ne pas répéter plus de 3 fois le même caractère",
-        "Voir résulats": "Voir résulats {n}"
+        "Voir résulats": "Voir résulats {n}",
+        "Je veux recevoir les nouveautés et des communications commerciales personnalisées de BERSHKA par e-mail ou d'autres moyens": "Je veux recevoir les nouveautés et des communications commerciales personnalisées de BERSHKA par e-mail ou d'autres moyens",
+        "J'ai lu et j'accepte les Conditions générales et je comprends les informations sur le traitement de mes données personnelles expliquées dans la": "J'ai lu et j'accepte les Conditions générales et je comprends les informations sur le traitement de mes données personnelles expliquées dans la"
     }
 })
