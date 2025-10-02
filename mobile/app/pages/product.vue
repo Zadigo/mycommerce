78ed3ac4e6a95f24701de113962e42b4ad2a43fd
Zadigo <pendenquejohn@gmail.com>
2025-10-03 01:39:34 +0200

    Improve mobile design

--- mobile/app/pages/product.vue
+++ mobile/app/pages/product.vue
@@ -9,7 +9,20 @@
     </ion-header>
 
     <ion-content>
-      Something
+      <simple-hero />
+
+      <ion-modal :is-open="true" breakpoints="[0, 0.15, 1]" initial-breakpoint="0.15"  handle>
+        <ion-content>
+          <h1>Hello, World!</h1>
+
+          <products-feed-two />
+        </ion-content>
+      </ion-modal>
     </ion-content>
   </ion-page>
 </template>
+
+<script lang="ts" setup>
+const { products } = useProductsFeed()
+provide('products', products)
+</script>
