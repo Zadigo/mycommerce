362648a9362ef5398af744cf03bf250712434940
Zadigo <pendenquejohn@gmail.com>
2026-05-07 22:36:51 +0200

    Update all packages

--- mymobile/lib/features/home/screens/home_screen.dart
+++ mymobile/lib/features/home/screens/home_screen.dart
@@ -7,13 +7,59 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
 
-    return Center(
-        child: Container(
-          height: 400,
+    return ListView(
+      children: [
+        Container(
+          height: 500,
           color: theme.colorScheme.primary,
           alignment: Alignment.topCenter,
-          child: Image(image: AssetImage('images/img1.jpg'),),
+          child: Image(
+            width: 550,
+            image: AssetImage('assets/images/img1.jpg'),
+            fit: BoxFit.cover,
+          ),
         ),
-      );
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              height: 200,
+              width: 200,
+              color: theme.colorScheme.primaryContainer,
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 8),
+              child: Image(
+                width: 550,
+                image: AssetImage('assets/images/img1.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: theme.colorScheme.primaryContainer,
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 8),
+              child: Image(
+                width: 550,
+                image: AssetImage('assets/images/img1.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+        Container(
+          height: 500,
+          color: theme.colorScheme.primary,
+          alignment: Alignment.topCenter,
+          margin: EdgeInsets.only(top: 8),
+          child: Image(
+            width: 550,
+            image: AssetImage('assets/images/img1.jpg'),
+            fit: BoxFit.cover,
+          ),
+        )
+      ],
+    );
   }
 }
