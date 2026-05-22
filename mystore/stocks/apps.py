cba3e33d372de2fa04eac85ac2a92dbacc735984
Zadigo <pendenquejohn@gmail.com>
2026-05-22 19:14:39 +0200

    Update mcp tools

--- mystore/stocks/apps.py
+++ mystore/stocks/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class StocksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'stocks'
+
+    def ready(self):
+        import stocks.mcp
